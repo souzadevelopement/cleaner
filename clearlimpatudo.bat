b6a050d0116b410ac00433042747e1d98afaf4a0
nexlygg <nexly11@proton.me>
2026-01-24 14:18:25 -0300

    limpadores

---
+++ clearlimpatudo.bat
@@ -0,0 +1,8 @@
+
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\cache"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\nui-storage"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache-priv"
+rmdir /s /q "%AppData%\CitizenFX\kvs"
+rmdir /s /q "%LocalAppData%\DigitalEntitlements"
+start https://discord.com/invite/randolas
